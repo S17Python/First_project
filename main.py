6a3bffa13c4848c26350a2fd441959c2a48846da
S17Python <Shulgasergey17@yandex.ru>
2024-07-08 13:21:50 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,138 @@
+# print("домашка")
+# print((5**2)+(12//3))
+# print((10/15)-20)
+# print('Это викторина на знание городов. Ответь на вопросы и узнай, насколько хорошо ты разбираешься в городах!')
+# question1="Как называется столица России: "
+# question2="Как называется столица Америки: "
+# question3="Как называется столица Франции: "
+# question4="Как называется столица Китая: "
+# question5="Как называется столица Бразилии: "
+# question6="В окрестностях какого города находится Пирамида Хеопса: "
+# question7="Как называется столица 2 по площади государства: "
+# true_answer1="Москва"
+# true_answer2="Вашингтон"
+# true_answer3="Париж"
+# true_answer4="Пекин"
+# true_answer5="Бразилиа"
+# true_answer6="Гиза"
+# true_answer7="Оттава"
+#
+# score=0
+#
+# answer1=input(question1)
+# if answer1==true_answer1:
+#     score+=1
+# answer2=input(question2)
+# if answer2==true_answer2:
+#     score+=1
+# answer3=input(question3)
+# if answer3==true_answer3:
+#     score+=1
+# answer4=input(question4)
+# if answer4==true_answer4:
+#     score+=1
+# answer5=input(question5)
+# if answer5==true_answer5:
+#     score+=1
+# answer6=input(question6)
+# if answer6==true_answer6:
+#     score+=1
+# answer7=input(question7)
+# if answer7==true_answer7:
+#     score+=1
+#
+# if score>=5:
+#     print("Вы набрали много баллов,вас можно считать экспертом")
+# else:
+#     print("Вы набрали мало  баллов,расширяйте свой кругозор,вам есть, куда стремится")
+
+# number=20
+# number2=2
+# print(number**number2)
+# number3=30
+# number4=7
+# print(number3//number4)
+# message="Я изучаю программирование"
+# print(message)
+# days=7
+# months=12
+# print(days)
+# print(months)
+# name="Сергей"
+# message="Ты станешь хорошим программистом,\nты уже начал учится"
+# print('Привет,',name ,'который из будуещего,',message)
+# name=input("Как тебя зовут: ")
+# lastname=input("Какая у тебя фамилия: ")
+# print("Привет",name,lastname)
+# product=input("Введите продукт: ")
+# money=int(input("Введите цену продукта: "))
+# print("Вы купили",product,"и потратили",money,"рублей")
+# phrase=input( "Введи фразу для запоминания порядка цветов в радуге:")
+# print(phrase)
+# year=int(input("Введи год рождения:"))
+# print(year)
+# a=10
+# b=str(a)
+# # # print(b)
+# # c=float(a)
+# # d=input(c)
+# # print(d)
+# x=type(b)
+# print(x)
+# a="hello "
+# b="world"
+# print(a+b)
+# a=float(input())
+# b=float(input())
+# c=int(a)+int(b)
+# print(c)
+# a=float(input())
+# b=int((a*10))
+# c=b//9
+# d=c//4
+# print(d)
+# wheather="дождь"
+# if wheather=="солнце":
+#     print("Одется лучше в летнею одежду")
+#
+# if wheather=="дождь":
+#     print("Лучше взять зонтик")
+# is_raining=True
+# if is_raining:
+#     print("Идёт дождь")
+# pasword=int(input("Пароль:"))
+# true_password=2435
+# if pasword==true_password:
+#     print("Верный пароль")
+# else:
+#     print("Неверный пароль")
+# a=int(input())
+# if a%2==0:
+#     print(1)
+# else:
+#     print(0)
+# a=5
+# if a%6**2==25:
+#     print(1)
+# else:
+#     if a%2**6 !=25:
+#         print(2)
+#     else:
+#         print(0)
+# a=int(input())
+# b=int(input())
+# print("Длина первой стороны:",a)
+# print("Длина второй стороны:",b)
+# print("Периметр:",(a+b)*2)
+# print("Площадь:",a*b)
+# if a==b:
+#     print("Это квадрат")
+# else:
+#     print("Это не квадрат")
+
+
+
+
+
+
+
